eba39797a5efe5cf0066abb60af283a12c0037b9
Penca53 <pencapencator@gmail.com>
2023-02-19 00:28:11 +0100

    Maybe?

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,13 @@ const config = {
  preprocess: vitePreprocess(),
 
  kit: {
-  adapter: adapter()
+  adapter: adapter(),
+  paths: {
+	base: dev ? '' : '/your-repo-name',
+  },
+  // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+  // For example, instead of '_app', use 'app_', 'internal', etc.
+  appDir: 'internal',
  }
 };
 
