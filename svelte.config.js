c7ef8cdde319308d387e526f9a14bc67d712490d
Penca53 <pencapencator@gmail.com>
2023-02-19 00:05:06 +0100

    Use gh-pages package

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,15 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+ // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+ // for more information about preprocessors
+ preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null,
-            precompress: false,
-			trailingSlash: 'always',
-			strict: true,
-        }),
-		paths: {
-		  base: dev ? '' : '/my-portfolio',
-		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal',
-	}
+ kit: {
+  adapter: adapter()
+ }
 };
 
 export default config;
