a381094bd9ceae9d6e915ad2dd06670fcc350adf
Penca53 <pencapencator@gmail.com>
2023-02-20 17:17:52 +0100

    Updates

---
+++ _app/immutable/chunks/0-7ef961b5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-64b8465d.js";import{default as t}from"../components/pages/_layout.svelte-d0b26541.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/2-a417a6a0.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/_page.svelte-f2c0e6f4.js";export{t as component,r as universal};
