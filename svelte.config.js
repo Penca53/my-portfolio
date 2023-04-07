a07ae509128fc5eb2858f0978e9c4f7bd7da0f40
Penca53 <pencapencator@gmail.com>
2023-04-07 23:31:39 +0200

    Update SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
